df680fbd49fc552e23653fd8efa7b4cbee41f1cc
Florence <plorenznl@gmail.com>
2024-10-21 23:20:00 +0800

    Start Pseudocode

--- ask_user_to_input_name_and_age.py
+++ ask_user_to_input_name_and_age.py
@@ -1,2 +1,15 @@
 # Pseudocodes
-# 1. 
+# 1. Start
+# 2. Initialize an empty list called people
+# 3. Loop1: Ask for input
+    # a. Loop2: Ask for a valid name
+        # i. IF name is not valid, display error and ask again
+    # b. Loop3: Ask for valid a valid age
+        # i. If age is not valid, diplay error and ask again
+    # c. Store name and age in the list
+    # d. Ask user if they want to add another entry (yes/no)
+    # e. If no, break the loop
+# 4. If the list is not empty, find the oldest person
+# 5. Display the oldest person's name and age
+# 6. End
+
