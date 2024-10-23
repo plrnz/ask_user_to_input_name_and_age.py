66ea14107ddb471e0d7c405c46297ec9e9bcbc01
Florence <plorenznl@gmail.com>
2024-10-24 00:22:52 +0800

    Check who is the oldest. Some error fixed

--- ask_user_to_input_name_and_age.py
+++ ask_user_to_input_name_and_age.py
@@ -1,47 +1,39 @@
-# Pseudocodes
-# 1. Start
-# 2. Initialize an empty list called people
-# 3. Loop1: Ask for input
-    # a. Loop2: Ask for a valid name
-        # i. IF name is not valid, display error and ask again
-    # b. Loop3: Ask for valid a valid age
-        # i. If age is not valid, diplay error and ask again
-    # c. Store name and age in the list
-    # d. Ask user if they want to add another entry (yes/no)
-    # e. If no, break the loop
-# 4. If the list is not empty, find the oldest person
-# 5. Display the oldest person's name and age
-# 6. End
-
 # List to store the name and age
-name: [] # type: ignore
-age: [] # type: ignore
+names = [] # List for storing names
+ages = [] # List for storing ages
 
 # Start of Loop
 # Loop 1: Ask for input
 while True:
     # Loop 2: Ask for a valid name
     while True:
-        name = input("Please input a name: ")
-        if name.isalpha(): # Upper case and lower case letters only
+        participants_names = input("Please input a name: ")
+        if participants_names.isalpha(): # Upper case and lower case letters only
             break
         else:
             print("Invalid name. Please input a valid name: ")
     
     # Loop 3: Ask the user to input a valid age
     while True:
-        age = input("Please Input age: ")
-        if age.isdigit(): # Numbers only. No special characters
-            age = int(age)
+        participants_ages = input("Please Input age: ")
+        if participants_ages.isdigit(): # Numbers only. No special characters
+            participants_ages = int(participants_ages)
             break
         else:
             print("Invalid age. Please input again: ")
 
     # Store the name and age in the list
-        name.append(name)
-        age.append(age)
+    names.append(participants_names)
+    ages.append(participants_ages)
 
     # Ask the user if they want to add another entry (yes/no)
-    retry = input("Do you want to add another participant? ('y/n): ").lower()
-    if retry == "n":
+    retry = input("Do you want to add another participant? (yes/no): ").lower()
+    if retry == "no":
         break
+
+# Check who is the oldest
+if len(names) > 0:
+    oldest_index = ages.index(max(ages))
+    print(f"The oldest person is {names[oldest_index]} with an age of {ages[oldest_index]}.")
+else:
+    print("No valid entries were addded.")
