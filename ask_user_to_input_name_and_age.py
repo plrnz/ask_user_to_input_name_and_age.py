14651eb490ff9232d22ce130b9473bf99d256373
Florence <plorenznl@gmail.com>
2024-10-24 13:53:34 +0800

    Allows the use of space, hyphens, and apostrophe for names

--- ask_user_to_input_name_and_age.py
+++ ask_user_to_input_name_and_age.py
@@ -1,7 +1,8 @@
 # Pseudocodes
 # 1. Start
 # 2. Initialize an empty list called people
-# 3. Loop1: Ask for input
+# 3. Allow special characters for the names
+# 4. Loop1: Ask for input
     # a. Loop2: Ask for a valid name
         # i. IF name is not valid, display error and ask again
     # b. Loop3: Ask for valid a valid age
@@ -9,21 +10,28 @@
     # c. Store name and age in the list
     # d. Ask user if they want to add another entry (yes/no)
     # e. If no, break the loop
-# 4. If the list is not empty, find the oldest person
-# 5. Display the oldest person's name and age
-# 6. End
+# 5. If the list is not empty, find the oldest person
+# 6. Display the oldest person's name and age
+# 7. End
 
 # List to store the name and age
 names = [] # List for storing names
 ages = [] # List for storing ages
 
+# Allow special characters for names
+def valid_name(name):
+    for char in name:
+        if not (char.isalpha() or char in [" ", "-", "'"]): # Allows space, hyphens, apostrophe
+            return False
+    return True
+    # 
 # Start of Loop
 # Loop 1: Ask for input
 while True:
     # Loop 2: Ask for a valid name
     while True:
         participants_names = input("Please input a name: ")
-        if participants_names.isalpha(): # Upper case and lower case letters only
+        if valid_name(participants_names): # Upper case letters, lower case letters, space, hyphens, and apostrophe cn be used
             break
         else:
             print("Invalid name. Please input a valid name: ")
